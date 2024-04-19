4cae23d749e8fd5f79514ec6a9f1fd03e0ce5423
marceloagoston <marceloagoston91@gmail.com>
2024-04-19 12:21:09 -0300

    feat: add vote model

--- ballot/voting/models.py
+++ ballot/voting/models.py
@@ -91,3 +91,28 @@ class PoliticalParty(models.Model):
             )
 
         super().delete(*args, **kwargs)
+
+
+class Vote(models.Model):
+    political_party = models.ForeignKey(
+        PoliticalParty, on_delete=models.PROTECT, null=True, blank=True, default=None
+    )
+    district = models.ForeignKey(District, on_delete=models.PROTECT, null=False)
+
+    def __str__(self):
+        return f'Voto a {self.political_party.party_name} - ({self.district})'
+
+    class Meta:
+        verbose_name = 'Vote'
+        verbose_name_plural = 'Votes'
+
+    def save(self, *args, **kwargs):
+        if ElectionHandler.objects.all().first().status != 'abi':
+            raise ValidationError(
+                'No se puede crear/modificar registros con la elección pendiente o finalizada '
+            )
+
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        raise ValidationError('No se puede eliminar votos')
